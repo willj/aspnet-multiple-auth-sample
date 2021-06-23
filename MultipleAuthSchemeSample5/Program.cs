f01183a4a619a52143feb1cccf533bfc50c1c8fc
Willj <willj@users.noreply.github.com>
2021-06-23 16:54:42 +0100

    load app config

--- MultipleAuthSchemeSample5/Program.cs
+++ MultipleAuthSchemeSample5/Program.cs
@@ -18,6 +18,10 @@ namespace MultipleAuthSchemeSample5
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile($"appconfig.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
